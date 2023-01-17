fcfd2fec24dd2f7cf1737150e8528b19da2f482c
Pablo Álvarez García <palvarga@emeal.nttdata.com>
2023-01-17 17:31:25 +0100

    Added StudentService's add method closes #9

--- NttData.FileManagement.Business.Logic/Implementations/StudentService.cs
+++ NttData.FileManagement.Business.Logic/Implementations/StudentService.cs
@@ -1,5 +1,7 @@
 ﻿using NttData.FileManagement.Business.Logic.Contracts;
 using NttData.FileManagement.Common.Model;
+using NttData.FileManagement.DataAccess.Repository.Contracts;
+using NttData.FileManagement.DataAccess.Repository.Implementations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +14,18 @@ namespace NttData.FileManagement.Business.Logic.Implementations
     {
         public bool Add(Student student)
         {
-            throw new NotImplementedException();
+            CalculaEdad(student);
+            IStudentRepository studentRepository = new StudentRepository();
+            studentRepository.Add(student);
+            return true;
+        }
+
+        void CalculaEdad(Student student)
+        {
+            var Ahora = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+            var Nacimiento = new DateTime(student.Birthday.Year, student.Birthday.Month, student.Birthday.Day);
+            var edad = ((Ahora - Nacimiento).Days / 365);
+            student.Age = edad;
         }
     }
 }
